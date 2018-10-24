a224e4f119e44521853816006adf0d6facd25e9f
Massimo Biagioli <m.biagioli@palinformatica.it>
2018-10-24 12:45:12 +0200

    Sistemate classi css

--- src/view/notify.php
+++ src/view/notify.php
@@ -1,11 +1,21 @@
-<div id="notify-container">
-    <div id="notify-title"><?=$this->title ?></div>
-    <div id="notify-message"><?=$this->message ?></div>
-    <div id="notify-actions">
-        <?php foreach ($this->actions as $action) : ?>
-        <div class="notify-action">
-            <span><a href="<?=$action['link']?>"</a><?=$action['title']?></span>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Notifica</title>
+        <link rel="icon" href="<?= App::getInstance()->getConfig('ROOT') ?>/img/favicon.png" type="image/png" />
+        <link rel="stylesheet" href="<?= App::getInstance()->getConfig('ROOT') ?>/css/style.css" type="text/css" />
+    </head>
+    <body>
+        <div id="notify-container">
+            <div id="notify-title"><?= $this->title ?></div>
+            <div id="notify-message"><?= $this->message ?></div>
+            <div id="notify-actions">
+                <?php foreach ($this->actions as $action) : ?>
+                    <div class="notify-action">
+                        <span><a href="<?= $action['link'] ?>"</a><?= $action['title'] ?></span>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
-        <?php endforeach; ?>
-    </div>
-</div>
+    </body>
+</html>
